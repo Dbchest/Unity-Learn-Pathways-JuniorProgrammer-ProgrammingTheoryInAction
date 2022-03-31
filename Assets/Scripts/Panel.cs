b779909247fa55ff1925bf77c0563f65b316e766
Derek <56432831+Dbchest@users.noreply.github.com>
2022-03-30 20:14:49 -0400

    Update

--- Assets/Scripts/Panel.cs
+++ Assets/Scripts/Panel.cs
@@ -17,23 +17,6 @@ public class Panel : MonoBehaviour
 
     public Image Image { get; private set; }
 
-    public float Opacity
-    {
-        get { return Image.color.a; }
-
-        set
-        {
-            var color = Image.color;
-
-            if (value < 0f || value > 1f)
-            {
-                Mathf.Clamp(value, 0f, 1f);
-            }
-
-            Image.color = new Color(color.r, color.g, color.b, value);
-        }
-    }
-
     public Animator Animator { get; private set; }
 
     public float DefaultSpeedMultiplier
@@ -89,16 +72,6 @@ public class Panel : MonoBehaviour
     public bool IsBusy
     {
         get { return Animator.GetBool("Is Busy"); }
-    }
-
-    public void SetOpacity(int alpha)
-    {
-        if (alpha < 0 || alpha > 255)
-        {
-            Mathf.Clamp(alpha, 0, 255);
-        }
-
-        Opacity = (float)alpha / 255;
     }
 
     public void Show()
